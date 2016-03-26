8f5d5ce1f89e83a8310515e41234baab4466aa44
Michael Hsu (徐承志) <evenchange4@gmail.com>
2016-03-26 10:23:59 +0800

    first commit

---
+++ eslint/index.js
@@ -0,0 +1,30 @@
+module.exports = {
+  "extends": [
+    "airbnb",
+    "plugin:ava/recommended"
+  ],
+  "plugins": [
+    "ava"
+  ],
+  "rules": {
+    "react/sort-comp": [2, {
+      "order": [
+        "lifecycle",
+        "/^on.+$/",
+        "/^(get|set)(?!(InitialState$|DefaultProps$|ChildContext$)).+$/",
+        "everything-else",
+        "/^render.+$/",
+        "render"
+      ]
+    }],
+    "object-shorthand": [2, "always"],
+    "object-curly-spacing": [2, "always", {
+      "objectsInObjects": false,
+      "arraysInObjects": false
+    }],
+    "react/jsx-curly-spacing": [2, "never", {"allowMultiline": true}],
+    "react/no-multi-comp": [2, { "ignoreStateless": true }],
+    "no-use-before-define": [2, "nofunc"],
+    "no-console": 2,
+  }
+};
